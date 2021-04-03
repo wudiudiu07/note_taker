d34a7c748ff3e436e3bced75d327745db379652f
Rocrossover <43283190+Rocrossover@users.noreply.github.com>
2021-04-02 22:41:34 -0400

    all parts included
    potential minor changes/improvements to make

--- deepspeech_start.py
+++ deepspeech_start.py
@@ -1,4 +1,3 @@
-
 import deepspeech
 import wave
 import re
@@ -18,6 +17,25 @@ import subprocess
 from tqdm import tqdm
 from audioProcessing import extract_audio, convert_samplerate
 import segmentAudio
+import truecase
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+
+def correct_output(in_file,out_file):
+    PUNCTUATION_MAPPING = {" !EXCLAMATIONMARK" : "!", " .PERIOD" : ".", " :COLON": ":", " ,COMMA" : ",", " ;SEMICOLON": ";" ," -DASH": "-"," ?QUESTIONMARK" : "?"}
+    in_f = open(in_file,'r')
+    out_f = open(out_file,'w')
+    contents = in_f.read()
+    for x in PUNCTUATION_MAPPING:
+        contents = contents.replace(x,PUNCTUATION_MAPPING[x])
+    contents = truecase.get_true_case(contents)
+    out_f.write(contents)
+    in_f.close()
+    out_f.close()
+
 
 def convert_video(filepath):
     base = ntpath.basename(filepath)
@@ -83,7 +101,7 @@ def to_deepspeech(filepath):
     #output text file
     audio_file_name = filepath.split(os.sep)[-1].split(".")[0]
     filename = ntpath.basename(filepath).split(".")[0]
-    txt_file_name = os.path.join(output_directory,filename+".txt")
+    txt_file_name = os.path.join(output_directory,filename+"_temp1.txt")
     file_handle = open(txt_file_name,"w+")
     
     #split silent parts in audio file
@@ -94,13 +112,77 @@ def to_deepspeech(filepath):
         audio_segment_path = os.path.join(audio_directory,file)
         ds_process_audio(ds, audio_segment_path,file_handle)
     
-    print("finished! output is",txt_file_name)
     file_handle.close()
     
-    ##clean directory and temp file
-    #shutil.rmtree(audio_directory)
-    #os.mkdir(audio_directory)
+    print("\nSpeech-to-Text Conversion Complete, restoring Punctuation...")
     
+    # Running commands to punctuate deepspeech output
+    punct2_script = os.path.join(base_directory,"punctutator2_theano\punctuator2-master\punctuator.py")
+    punct2_model = os.path.join(base_directory,"punctutator2_theano\models\INTERSPEECH-T-BRNN.pcl")
+    punct_output = os.path.join(output_directory,filename+"_temp2.txt")
+    final_output = os.path.join(output_directory,filename+".txt")
+    
+    subprocess.run(["python2",punct2_script,punct2_model,punct_output,txt_file_name]) #punct_output_txt
+    correct_output(punct_output,final_output)
+    
+    
+    print("\nfinished! output is",final_output,"\n") #final_output_txt
+    print("Summarization starts...\n")
+    
+    
+    # deleting original, unpuncutatued deepspeech output
+    if os.path.exists(txt_file_name):
+      os.remove(txt_file_name)
+    else:
+      print("The file does not exist")
+    
+    if os.path.exists(punct_output):
+      os.remove(punct_output)
+    else:
+      print("The file does not exist")
+        
+    
+    # Text Summarization
+    LANGUAGE = "english"
+    f = open(final_output,'r')
+    lines = f.read().find(".")
+    #print(lines,"\n")
+    f.close()
+    
+    SENTENCES_COUNT = int(lines/4) #Quarter of the content
+    # print(SENTENCES_COUNT,"\n")
+
+    parser = PlaintextParser.from_file(final_output, Tokenizer(LANGUAGE))
+    stemmer = Stemmer(LANGUAGE)
+
+    summarizer = Summarizer(stemmer)
+    summarizer.stop_words = get_stop_words(LANGUAGE)
+    Final = os.path.join(output_directory,filename+"_Final.txt")
+    F = open(Final,"w")
+    
+    print("Your Output will be shown below and saved in the output directory.\n")
+    for sentence in summarizer(parser.document, SENTENCES_COUNT):
+        print(sentence)
+        print(sentence, file=F)
+    
+    F.close()
+    print("\nEverything is done!")
+    
+    ##clean directory and temp file
+    os.remove(os.path.join(base_directory,filename + ".wav"))
+    shutil.rmtree(audio_directory)
+    os.mkdir(audio_directory)
+    
+    
+    #if os.path.exists(final_output):
+        #os.remove(final_output)
+    #else:
+        #print("The file does not exist")
+        
+    #os.rename(tmp_f,os.path.join(output_directory,filename+".txt"))
+    
+
+ 
 def main():
     filepath = sys.argv[3]#path, get rid of 'File Path'
     
